89be0aae111ebe3adee245dc50b41c885c0a6972
Garrett Walter <gtwalter150@gmail.com>
2020-11-17 18:13:18 -0500

    using * made the index.html persist and did not allow /notes to be displayed to the page

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var path = require("path");
 //     res.sendFile(path.join(__dirname, "index.html"));
 //   });
 
-  app.get("*", function(req, res) {
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
   
