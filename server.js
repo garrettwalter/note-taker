9c548e8f98913cd4bbec68ff7c865ffb287f7a3d
Garrett Walter <gtwalter150@gmail.com>
2020-11-17 17:11:37 -0500

    put routes in server.js can not get site to deploy properlly

--- server.js
+++ server.js
@@ -10,9 +10,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Sets up the Express app to handle data parsing
-require("./code/routes/apiRoutes")(app);
-require("./code/routes/htmlRoutes")(app);
+// require("./code/routes/apiRoutes")(app);
+// require("./code/routes/htmlRoutes")(app);
 
+var path = require("path");
+
+app.get("*", function(req, res) {
+    res.json(path.join(__dirname, "./code/index.html"));
+  });
+
+  app.get("/", function(req, res) {
+    res.json(path.join(__dirname, "/code/index.html"));
+  });
+  
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/code/notes.html"));
+  });
 // =============================================================================
 // LISTENER
 // The below code effectively "starts" our server
