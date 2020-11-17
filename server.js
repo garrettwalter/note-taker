c1fff82b2c695c27045102a6c057fdcc86e44cd7
Garrett Walter <gtwalter150@gmail.com>
2020-11-17 14:37:46 -0500

    creates files for routes

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var path = require("path");
 
 // Sets up the Express App
 // =============================================================
@@ -7,17 +6,17 @@ var app = express();
 var PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
+require("./code/routes/apiRoutes")(app);
+require("./code/routes/htmlRoutes")(app);
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("*", function(req, res) {
-  res.json(path.join(__dirname, "/code/index.html"));
-});
-
-app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "/code/notes.html"));
-});
-
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
