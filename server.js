f76252172cd956614e8660ec11ed6e9ea2a66dd9
Garrett Walter <gtwalter150@gmail.com>
2020-11-18 16:29:24 -0500

    deleted route files, put routes in server.js file

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var PORT = process.env.PORT || 3001;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
 // Sets up the Express app to handle data parsing
 // require("./code/routes/apiRoutes")(app);
