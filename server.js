59af2502a8333d9a4b4c0bb8fe86847327556d33
Garrett Walter <gtwalter150@gmail.com>
2020-11-18 17:25:27 -0500

    fixed routes

--- server.js
+++ server.js
@@ -20,12 +20,16 @@ var path = require("path");
 //     res.sendFile(path.join(__dirname, "index.html"));
 //   });
 
-  app.get("*", function(req, res) {
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
   });
   
   app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
+  });
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
   });
 
   //  ===============================================================================
