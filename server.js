8b89c0256ebc100d4cedc4f8d8d05d8026889f98
Garrett Walter <gtwalter150@gmail.com>
2020-11-18 16:45:08 -0500

    added './' to the front of all file links

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var PORT = process.env.PORT || 3001;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
+
 
 // Sets up the Express app to handle data parsing
 // require("./code/routes/apiRoutes")(app);
@@ -20,12 +20,12 @@ var path = require("path");
 //     res.sendFile(path.join(__dirname, "index.html"));
 //   });
 
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/index.html"));
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./index.html"));
   });
   
   app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/notes.html"));
+    res.sendFile(path.join(__dirname, "./notes.html"));
   });
 
   //  ===============================================================================
@@ -34,7 +34,7 @@ var path = require("path");
   // These data sources hold arrays of information on table-data, waitinglist, etc.
   // ===============================================================================
   
-  var notesData = require("/db.json");
+  var notesData = require("./db.json");
 
   app.get("/api/notes", function(req, res) {
     res.json(notesData);
